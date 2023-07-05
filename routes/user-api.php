4c2d11d453cfd3a8cb5076b6c9b7ea0d1314a893
nz-hridoy <nayeemuzaman05@gmail.com>
2023-07-05 21:56:07 +0600

    middleware + api

--- routes/user-api.php
+++ routes/user-api.php
@@ -1,15 +1,16 @@
 <?php
 
 use App\Http\Controllers\api\user\auth\AuthenticationController;
+use App\Http\Controllers\api\user\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::post('v1/login', [AuthenticationController::class, 'login']);
 
 //Authenticated user
-Route::group(['middleware' => ['jwtUser:user-api', 'jwt.auth'], 'prefix' => 'v1/dashboard'], function () {
+Route::group(['middleware' => ['jwtUser:user-api', 'jwt.auth'], 'prefix' => 'v1/user'], function () {
     // Route::post('logout', [UserAuthenticationController::class, 'userLogout']);
 
-    //Supports
-    // Route::get('support-tickets', [SupportController::class, 'index']);
+    //User Profile
+    Route::get('profile', [DashboardController::class, 'userInfo']);
 });
