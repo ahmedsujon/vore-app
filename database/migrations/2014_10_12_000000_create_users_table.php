6915bd4f57cbf6b5e2b613ad75146d8d9ec3f94c
nz-hridoy <nayeemuzaman05@gmail.com>
2023-07-05 21:03:27 +0600

    migrations + model setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,20 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('gender')->nullable();
+            $table->string('goal', 2000)->nullable();
+            $table->string('daily_activity_level', 2000)->nullable();
+            $table->double('current_weight', 20,2)->default(0);
+            $table->enum('current_weight_unit', ['lbs','kg'])->default('kg');
+            $table->double('target_weight', 20,2)->default(0);
+            $table->enum('target_weight_unit', ['lbs','kg'])->default('kg');
+            $table->double('height', 20,2)->default(0);
+            $table->enum('height_unit', ['ft','cm'])->default('ft');
+            $table->string('birth_date')->nullable();
+            $table->longText('measurements')->nullable();
+            $table->enum('measurements_unit', ['in','cm'])->default('in');
             $table->rememberToken();
+            $table->integer('suspended')->default(0);
             $table->timestamps();
         });
     }
