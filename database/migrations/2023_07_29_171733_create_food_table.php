635a9f9b9d306add48b99c97fef11947e6159bdf
nz-hridoy <nayeemuzaman05@gmail.com>
2023-07-29 23:43:24 +0600

    food table seeder

--- database/migrations/2023_07_29_171733_create_food_table.php
+++ database/migrations/2023_07_29_171733_create_food_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('food', function (Blueprint $table) {
+        Schema::create('foods', function (Blueprint $table) {
             $table->id();
             $table->enum('added_by', ['user', 'admin'])->nullable();
             $table->bigInteger('user_id')->unsigned()->nullable();
@@ -34,6 +34,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('food');
+        Schema::dropIfExists('foods');
     }
 };
