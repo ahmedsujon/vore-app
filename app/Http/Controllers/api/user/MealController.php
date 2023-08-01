da36631920772584c7ec64fca21f4a543e83b48d
nz-hridoy <nayeemuzaman05@gmail.com>
2023-08-01 22:03:47 +0600

    modal + migrations

--- app/Http/Controllers/api/user/MealController.php
+++ app/Http/Controllers/api/user/MealController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Validator;
 
 class MealController extends Controller
 {
+    // Breakfast
     public function breakfastIndex(Request $request)
     {
         try {
@@ -54,6 +55,56 @@ class MealController extends Controller
             $breakfast->save();
 
             return response()->json(['result' => 'true', 'message' => 'Breakfast added successfully']);
+        } catch (Exception $ex) {
+            return response($ex->getMessage());
+        }
+    }
+
+    // Lunch
+    public function lunchIndex(Request $request)
+    {
+        try {
+            $lunch = Lunch::where('id', $request->lunch_id)->where('user_id', api_user()->id)->first();
+
+            if ($lunch) {
+                return response()->json($lunch);
+            } else {
+                return response()->json(['result' => 'false', 'message' => 'No data found!']);
+            }
+        } catch (Exception $ex) {
+            return response($ex->getMessage());
+        }
+    }
+
+    public function addlunch(Request $request)
+    {
+        $rules = [
+            'foods' => 'required',
+            'total_calories' => 'required',
+            'total_protein' => 'required',
+            'total_crabs' => 'required',
+            'total_fat' => 'required',
+            'date' => 'required',
+
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        try {
+            $lunch = new Lunch();
+            $lunch->user_id = api_user()->id;
+            $lunch->foods = $request->foods;
+            $lunch->total_calories = $request->total_calories;
+            $lunch->total_protein = $request->total_protein;
+            $lunch->total_crabs = $request->total_crabs;
+            $lunch->total_fat = $request->total_fat;
+            $lunch->date = $request->date;
+            $lunch->status = 1;
+            $lunch->save();
+
+            return response()->json(['result' => 'true', 'message' => 'Lunch added successfully']);
         } catch (Exception $ex) {
             return response($ex->getMessage());
         }
---
+++ database/migrations/2023_08_01_160136_create_snacks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('snacks', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->string('foods')->nullable();
+            $table->double('total_calories', 20,2)->default(0);
+            $table->double('total_protein', 20,2)->default(0);
+            $table->double('total_crabs', 20,2)->default(0);
+            $table->double('total_fat', 20,2)->default(0);
+            $table->date('date')->nullable();
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('snacks');
+    }
+};
