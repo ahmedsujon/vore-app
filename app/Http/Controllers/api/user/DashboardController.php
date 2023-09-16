e1416ffe2814521cf0dc68407ead69a0fd6a5af8
nz-hridoy <nayeemuzaman05@gmail.com>
2023-09-16 18:46:10 +0600

    measurements api update

--- app/Http/Controllers/api/user/DashboardController.php
+++ app/Http/Controllers/api/user/DashboardController.php
@@ -14,6 +14,7 @@ use Illuminate\Http\Request;
 use App\Models\BreakfastFood;
 use App\Models\UserActivityItem;
 use App\Http\Controllers\Controller;
+use App\Models\Measurement;
 use App\Models\User;
 use App\Models\WaterSetting;
 use Illuminate\Support\Facades\Validator;
@@ -312,6 +313,7 @@ class DashboardController extends Controller
     {
         $rules = [
             'weight' => 'required',
+            'date' => 'required',
         ];
         $validator = Validator::make($request->all(), $rules);
         if ($validator->fails()) {
@@ -327,6 +329,18 @@ class DashboardController extends Controller
             }
             $user->current_weight = $weight;
             $user->save();
+
+            $getMeasurement = Measurement::where('user_id', api_user()->id)->where('date', Carbon::parse($request->date)->format('Y-m-d'))->first();
+            if($getMeasurement){
+                $mes = $getMeasurement;
+                $mes->weight = $request->weight / 2.20462;
+            } else {
+                $mes = new Measurement();
+                $mes->user_id = api_user()->id;
+                $mes->date = $request->date;
+                $mes->weight = $request->weight / 2.20462;
+            }
+            $mes->save();
 
             return response()->json(['result' => 'true', 'message' => 'Data updated successfully']);
 
