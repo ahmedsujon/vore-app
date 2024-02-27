81730bccccb8de5574bc79a94a2496ac4e95366b
nz-hridoy <nayeemuzaman05@gmail.com>
2024-02-27 16:35:10 +0600

    admin auth setup

---
+++ app/Livewire/Admin/Layouts/Inc/Header.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Layouts\Inc;
+
+use Livewire\Component;
+
+class Header extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.layouts.inc.header');
+    }
+}
