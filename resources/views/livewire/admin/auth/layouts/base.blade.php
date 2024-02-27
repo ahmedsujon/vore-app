da0f7d20f0d73ce1b7a9f5b68a7cd13bd0b885e2
nz-hridoy <nayeemuzaman05@gmail.com>
2024-02-27 16:43:47 +0600

    assets + Login

--- resources/views/livewire/admin/auth/layouts/base.blade.php
+++ resources/views/livewire/admin/auth/layouts/base.blade.php
@@ -1,3 +1,37 @@
-<div>
-    {{-- The whole world belongs to you. --}}
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Vore - Admin Login</title>
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('assets/admin/images/favicon.ico') }}">
+
+    <!-- Styles CSS -->
+    <link href="{{ asset('assets/admin/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/admin/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+
+    <!-- App Css -->
+    <link href="{{ asset('assets/admin/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+
+    @livewireStyles
+</head>
+<body>
+
+    {{ $slot }}
+
+    <!-- JAVASCRIPT -->
+    <script src="{{ asset('assets/admin/libs/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/libs/metismenu/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/libs/node-waves/waves.min.js') }}"></script>
+
+    <!-- App js -->
+    <script src="{{ asset('assets/admin/js/app.js') }}"></script>
+
+    @livewireScripts
+</body>
+</html>
