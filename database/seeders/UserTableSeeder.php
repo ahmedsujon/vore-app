b7a14dd120fc4a57408ace2bdeeda3febf0fd1ed
nz-hridoy <nayeemuzaman05@gmail.com>
2024-03-18 22:16:45 +0600

    img fix

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -88,7 +88,7 @@ class UserTableSeeder extends Seeder
                         $mes->icon = 'assets/app/measurements/muscle.png';
                     } else if ($key == 5) {
                         $mes->icon = 'assets/app/measurements/body_fat.png';
-                    } else if ($key == 2) {
+                    } else if ($key == 6) {
                         $mes->icon = 'assets/app/measurements/muscle_mass.png';
                     }
                     $mes->save();
