7b8ae0fd624526082c68a1f6660ecfe6fd36dcfa
ahmedsujon <sujonahmed424@gmail.com>
2024-05-07 15:01:08 +0600

    update feedbacks

--- app/Livewire/App/Pages/AboutComponent.php
+++ app/Livewire/App/Pages/AboutComponent.php
@@ -3,9 +3,11 @@
 namespace App\Livewire\App\Pages;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
 class AboutComponent extends Component
 {
+    #[Title('Vore - About Us')]
     public function render()
     {
         return view('livewire.app.pages.about-component')->layout('livewire.app.layouts.base');
